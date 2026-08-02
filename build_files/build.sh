92b2a9cf2a606b061c5f24362e684eb0128043d3
Edward Groves <EdwardGroves2007@proton.me>
2026-08-02 12:05:20 -0400

    Feat: kitty

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ cp -avf "/ctx/system_files"/. /
 
 dnf5 -y copr enable lionheartp/Hyprland
 
-dnf5 install -y tmux noctalia hyprland
+dnf5 install -y tmux kitty noctalia hyprland
 
 dnf5 -y copr disable lionheartp/Hyprland
 
