903c1dbe44af6bda61c3fb71309b54e389c4cbc4
Edward Groves <EdwardGroves2007@proton.me>
2026-08-04 13:03:33 -0400

    too many packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,4 +4,10 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files"/. /
 
-pacman -Syu --noconfirm ghostty git waybar rofi
+pacman -Syu --noconfirm ghostty git waybar rofi mate-polkit flatpak pipewire pavucontrol wireplumber pipewire fastfetch pipewire-pulse bluetui bluetui steam base-devel alacritty firefox docker docker-compose docker-buildx ollama-cuda niri
+
+systemctl --user enable pipewire
+systemctl --user enable pipewire-pulse
+systemctl --user enable wireplumber
+systemctl enable bluetooth.service
+systemctl enable docker.service
