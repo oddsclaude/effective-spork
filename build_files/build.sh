a763cbf97505df3f93dd1e2729869b2eda7cae78
Edward Groves <EdwardGroves2007@proton.me>
2026-08-04 11:28:55 -0400

    Feat: ghostty

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,12 +4,4 @@ set -ouex pipefail
 
 cp -avf "/ctx/system_files"/. /
 
-curl -fsSL https://install.determinate.systems/nix | sh -s -- install --no-confirm --init none
-
-dnf5 -y copr enable lionheartp/Hyprland
-
-dnf5 install -y tmux kitty noctalia waybar mate-polkit swaybg hyprland
-
-dnf5 -y copr disable lionheartp/Hyprland
-
-systemctl enable podman.socket
+pacman -Syu --noconfirm ghostty git waybar rofi
